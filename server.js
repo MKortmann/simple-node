28810816c6537c0c9884d0becfa98e781646c6c2
MKortmann <companymvms@gmail.com>
2021-01-09 22:25:14 +0100

    add: variables

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+
+const FAVORITE_INSTRUCTOR = process.env.NAME;
+
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`The variable is ${FAVORITE_INSTRUCTOR}`);
     await sleep(5000);
   }
 }
